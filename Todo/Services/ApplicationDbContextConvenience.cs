39e04ab83497cc3c9e7da0655db61edba860e763
Andrey Gubal <andrew.gubal@barberly.com>
2022-04-25 09:28:44 +0200

    #2 Order list items by importance

--- Todo/Services/ApplicationDbContextConvenience.cs
+++ Todo/Services/ApplicationDbContextConvenience.cs
@@ -17,14 +17,15 @@ namespace Todo.Services
         public static TodoList SingleTodoList(this ApplicationDbContext dbContext, int todoListId)
         {
             return dbContext.TodoLists.Include(tl => tl.Owner)
-                .Include(tl => tl.Items)
+                .Include(tl => tl.Items.OrderBy(i=>i.Importance))
                 .ThenInclude(ti => ti.ResponsibleParty)
                 .Single(tl => tl.TodoListId == todoListId);
         }
 
         public static TodoItem SingleTodoItem(this ApplicationDbContext dbContext, int todoItemId)
         {
-            return dbContext.TodoItems.Include(ti => ti.TodoList).Single(ti => ti.TodoItemId == todoItemId);
+            return dbContext.TodoItems.Include(ti => ti.TodoList)
+                .Single(ti => ti.TodoItemId == todoItemId);
         }
     }
 }
